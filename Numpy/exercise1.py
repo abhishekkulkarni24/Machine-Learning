9687da47ec3aa746d2de49eb661422a42afff067
Abhishek Kulkarni <kulkarni.abhishek2407@gmail.com>
2021-03-17 22:46:13 +0530

    Program Updated

---
+++ Numpy/exercise1.py
@@ -0,0 +1,58 @@
+'''
+
+You are given a number N and following by an array of N numbers and followed by two
+elements U and V. Find the minimum distance between the elements U and V in the array.
+The array may have duplicates.
+For example, if the array is (1, 5, 3, 7, 2, 8, 3, 4, 5, 9, 9, 3, 1, 3, 2, 9)
+Minimum distance (U=4, V=7): 4
+Input Format:
+First Line contains integer representing number of elements N.
+Second line contains N elements separated by spaces.
+Third line contains two numbers separated by spaces representing U and V.
+Output Format:
+Output displays integer representing minimum distance
+
+Sample Testcases:
+Input 1
+16
+1 5 3 7 2 8 3 4 5 9 9 3 1 3 2 9
+4 7
+Output 1
+4
+Input 2
+16
+1 5 3 7 2 8 3 4 5 9 9 3 1 3 2 9
+2 5
+Output 2
+3
+
+'''
+
+no = input()
+
+arr = list(map(int, input().split()))
+
+nos = list(map(int, input().split()))
+
+
+l1 = [i for i, x in enumerate(arr) if x == nos[0]]
+
+l2 = [i for i, x in enumerate(arr) if x == nos[1]]
+
+min = -5
+
+for i in l1:
+    for j in l2:
+        if min == -5:
+            min = i-j
+            if min < 0:
+                min *=-1
+        else:
+            t = i-j   
+            if t < 0:
+                t *=-1
+            if t < min:
+                min = t   
+
+print(min)                  
+
